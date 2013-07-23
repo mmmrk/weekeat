adb428575611f6bc3943617b7740bd2135c92206
Markus Waldén <markus.walden@gmail.com>
2013-07-23 20:22:09 +0200

    abstracted #add_meal_input to .tab_container. Changed navigation to js from css :target

---
+++ js/main.js
@@ -0,0 +1,52 @@
+var tabbed_container = {
+	set_vars: function () {
+		this.$container = $('.tabbed_container');
+		this.$tabs = this.$container.find('.tab');
+		this.$tab_content = this.$container.find('.tab_content');
+	},
+	navigate: function (tab) {
+		var previous = this.$tabs.filter('.active').add(this.$tab_content.filter('.active')),
+			index = tab.index(),
+			next = this.$tabs.eq(index).add(this.$tab_content.eq(index));
+
+		previous.removeClass('active');
+		next.addClass('active');
+		
+	},
+	part_toggle: function (toggler) {
+		var query = toggler.data('part').toString().split('.'),
+			target = this.$tab_content.eq(query[0] - 1).find('.content_part_' + query[1]);
+
+		target.addClass('active_part').siblings('.active_part').removeClass('active_part');
+	},
+	init: function () {
+		var self = this;
+
+		this.set_vars();
+
+		this.$container.on('click', '.tab', function (e) {
+			e.preventDefault();
+
+			if (!$(this).is('.active'))
+				self.navigate($(this));
+		});
+
+		this.$container.on('click', '.tab_nav_link', function (e) {
+			e.preventDefault();
+
+			var query = $(this).attr('href');
+
+			self.$tabs.find('a').filter('[href=' + query + ']').trigger('click');
+		});
+
+		this.$container.on('click', '.content_part_toggle', function (e) {
+			e.preventDefault();
+
+			self.part_toggle($(this));
+		});
+	}
+};
+
+$(function () {
+	tabbed_container.init();
+});
--- views/meal/add.php
+++ views/meal/add.php
@@ -18,54 +18,56 @@
 	</div>
 <?php } ?>
 
-<div id="add_meal_input">
-	<div id="add_meal_input_header">
+<div id="add_meal_input" class="tabbed_container">
+	<div class="tabbed_container_header">
 		<h1>Add meal</h1>
-		<nav id="add_meal_input_navigation">
+		<nav class="tab_container">
 			<ol>
-				<li><a href="#add_meal_step_1">1. Select method</a></li>
-				<li><a href="#add_meal_step_2">2. Select dish</a></li>
-				<li><a href="#add_meal_step_3">3. Add meal details</a></li>
+				<li class="tab active"><a href="#add_meal_step_1">1. Select method</a></li>
+				<li class="tab"><a href="#add_meal_step_2">2. Select dish</a></li>
+				<li class="tab"><a href="#add_meal_step_3">3. Add meal details</a></li>
 			</ol>
 		</nav>
 	</div>
 	
-	<div id="add_meal_step_1" class="add_meal_step">
-		<h2>1. Select method</h2>
+	<div class="tabbed_container_body">
+		<div id="add_meal_step_1" class="tab_content active">
+			<h2>1. Select method</h2>
 
-		<a href="#add_meal_step_2" class="add_meal_method_select" data-method="add_meal_step_2.1">Add from list</a>
-		<a href="#add_meal_step_2" class="add_meal_method_select" data-method="add_meal_step_2.2">Add from form</a>
-	</div>
-	
-	<div id="add_meal_step_2" class="add_meal_step">
-		<div id="add_meal_no_method">
-			<p>
-				Go to <a href="#add_meal_step_1">step 1</a> to select input method
-			</p>
+			<a href="#add_meal_step_2" class="content_part_toggle tab_nav_link" data-part="2.1">Add from list</a>
+			<a href="#add_meal_step_2" class="content_part_toggle tab_nav_link" data-part="2.2">Add from form</a>
 		</div>
+		
+		<div id="add_meal_step_2" class="tab_content">
+			<div class="content_part content_part_1">
+				<h2>2. Add from list</h2>
+		
+				<?php
+					require('views/dish/dish_list.php'); 
+				?>
+			</div>
 
-		<div class="step_part step_part_1">
-			<h2>2. Add from list</h2>
-	
-			<?php
-				require('views/dish/dish_list.php'); 
-			?>
+			<div class="content_part content_part_2">
+				<?php
+					$form_dish_heading = '2. Add from form';
+					require('views/dish/form_dish.php');
+				?>
+			</div>
+
+			<div class="no_part_chosen">
+				<p>
+					Go to <a class="tab_nav_link" href="#add_meal_step_1">step 1</a> to select input method
+				</p>
+			</div>
 		</div>
+		
+		<div id="add_meal_step_3" class="tab_content">
+			<h2>3. Meal details</h2>
 
-		<div class="step_part step_part_2">
-			<?php
-				$form_dish_heading = '2. Add from form';
-				require('views/dish/form_dish.php');
-			?>
-		</div>
-	</div>
-	
-	<div id="add_meal_step_3" class="add_meal_step">
-		<h2>3. Meal details</h2>
-
-		<div class="input_group">
-			<label for="meal_details_shopping_list">Shopping list</label>
-			<textarea id="meal_details_shopping_list" cols="75" rows="2" placeholder="Eg: potatoes, olive oil, salt"></textarea>
+			<div class="input_group">
+				<label for="meal_details_shopping_list">Shopping list</label>
+				<textarea id="meal_details_shopping_list" cols="75" rows="2" placeholder="Eg: potatoes, olive oil, salt"></textarea>
+			</div>
 		</div>
 	</div>
 </div>
