e020a0f81d8b8437b26cb60be98982a1400d5f9f
mmmrk <markus.walden@gmail.com>
2013-11-24 00:56:05 +0100

    input suggestions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var tab_container = {
-	set_vars: function () {
-		this.$container = $('.tab_container');
+	set_vars: function (container) {
+		this.$container = container;
 		this.$tabs = this.$container.find('.tab');
 		this.$tab_content = this.$container.find('.tab_content');
 		this.$nav_buttons = this.$container.find('.tab_container_footer .button');
@@ -37,10 +37,13 @@ var tab_container = {
 
 		target.addClass('active_part').siblings('.active_part').removeClass('active_part');
 	},
-	init: function () {
+	init: function (container) {
 		var self = this;
 
-		this.set_vars();
+		if (!container.length)
+			return;
+
+		this.set_vars(container);
 
 		this.$container.on('click', '.tab', function (e) {
 			e.preventDefault();
@@ -76,7 +79,7 @@ var tab_container = {
 	}
 },
 meal_form = {
-	set_vars: function () {
+	set_vars: function (form) {
 		this.$dish_id = $('#dish_id_value');
 		this.$dish_list = $('#dish_list');
 	},
@@ -84,16 +87,47 @@ meal_form = {
 		var previous = this.$dish_id.val();
 		this.$dish_id.val((previous != id) ? id : '');
 	},
-	init: function () {
-		this.set_vars();
+	init: function (form) {
+		if (!form.length)
+			return;
+		this.set_vars(form);
 		this.$dish_list.on('click', '[data-dish-id]', function () {
 			meal_form.select_dish_from_list($(this).data('dish-id'));
 			$(this).toggleClass('selected').siblings('.selected').removeClass('selected');
 		});
 	}
+},
+input_suggestions = {
+	set_vars: function (input_group) {
+		this.input_group = input_group;
+		this.input = this.input_group.find(':input');
+		this.suggestions = this.input_group.find('.suggestion_list');
+	},
+	toggle_suggestions: function (state) {
+		this.suggestions.toggle(state);
+	},
+	set_value: function (value) {
+		this.input.val(value);
+	},
+	init: function (input_group) {
+		if (!input_group.length)
+			return;
+		var self = this;
+		this.set_vars(input_group);
+		this.input.on('focus', function (event) {
+			self.toggle_suggestions(true);
+		});
+		this.suggestions.on('click', function (event) {
+			console.log(event.target.textContent);
+			self.input.val(event.target.textContent);
+			self.input.blur();
+			self.toggle_suggestions(false);
+		});
+	}
 };
 
 $(function () {
-	tab_container.init();
-	meal_form.init();
+	tab_container.init($('.tab_container'));
+	meal_form.init($('#add_meal_form'));
+	input_suggestions.init($('.with_suggestions'));
 });
--- views/meal/_form.php
+++ views/meal/_form.php
@@ -1,52 +1,57 @@
-					<?php
-						if ( isset($form_meal) && $form_meal['error'] )
-							echo '<p class="error">' . $form_meal['error']['id'] . '. ' . $form_meal['error']['message'] . '</p>';
-						//else if ( isset($form_meal) && !$form_meal['error'] ) {
-						//	$ready_dishes = $form_meal['ready_dishes'];
-						//	$ready_tags = $form_meal['ready_tags'];
-					?>
-					<form id="add_meal_form" action="<?= $_SERVER['PHP_SELF'] . '?section=meal&page=add&action=create&date=' . $app->display_date; ?>" method="post">
-						<input type="hidden" id="add_method_value" name="add_method" value="2" />
-						<input type="hidden" id="dish_id_value" name="dish[id]" value="0" />
+<?php
+	if ( isset($form_meal) && $form_meal['error'] )
+		echo '<p class="error">' . $form_meal['error']['id'] . '. ' . $form_meal['error']['message'] . '</p>';
+	//else if ( isset($form_meal) && !$form_meal['error'] ) {
+	//	$ready_dishes = $form_meal['ready_dishes'];
+	//	$ready_tags = $form_meal['ready_tags'];
+?>
+<form id="add_meal_form" action="<?= $_SERVER['PHP_SELF'] . '?section=meal&page=add&action=create&date=' . $app->display_date; ?>" method="post">
+	<input type="hidden" id="add_method_value" name="add_method" value="2" />
+	<input type="hidden" id="dish_id_value" name="dish[id]" value="0" />
 
-						<div id="add_meal_step_2" class="tab_content">
-							<div class="content_part content_part_1">
-								<h2>2. Add from list</h2>
+	<div id="add_meal_step_2" class="tab_content">
+		<div class="content_part content_part_1">
+			<h2>2. Add from list</h2>
 
-								<?php
-									require('views/dish/_list.php');
-								?>
-							</div>
+			<?php
+				require('views/dish/_list.php');
+			?>
+		</div>
 
-							<div class="content_part content_part_2">
-								<h2>2. Add from form</h2>
+		<div class="content_part content_part_2">
+			<h2>2. Add from form</h2>
 
-								<?php
-									require('views/dish/_form.php');
-								?>
-							</div>
+			<?php
+				require('views/dish/_form.php');
+			?>
+		</div>
 
-							<div class="no_part_chosen">
-								<h2>2. <i>No input method chosen</i></h2>
+		<div class="no_part_chosen">
+			<h2>2. <i>No input method chosen</i></h2>
 
-								<p>
-									Go to <a class="tab_nav_link" href="#add_meal_step_1">step 1</a> to select input method
-								</p>
-							</div>
-						</div>
+			<p>
+				Go to <a class="tab_nav_link" href="#add_meal_step_1">step 1</a> to select input method
+			</p>
+		</div>
+	</div>
 
-						<div id="add_meal_step_3" class="tab_content">
-							<h2>3. Meal details</h2>
+	<div id="add_meal_step_3" class="tab_content">
+		<h2>3. Meal details</h2>
 
-							<div class="input_group required">
-								<label for="meal[name]">Meal name</label>
-								<input name="meal[name]" type="text" value="dinner">
-							</div>
+		<div class="input_group required with_suggestions">
+			<label for="meal[name]">Meal name</label>
+			<input name="meal[name]" type="text" value="dinner">
+			<ul class="suggestion_list">
+				<li class="suggestion_item">dinner</li>
+				<li class="suggestion_item">lunch</li>
+				<li class="suggestion_item">breakfast</li>
+			</ul>
+		</div>
 
-							<div class="input_group">
-								<label for="meal[shopping_list]">Shopping list</label>
-								<textarea name="meal[shopping_list]" cols="75" rows="2" placeholder="Eg: potatoes, olive oil, salt"></textarea>
-							</div>
-						</div>
-					</form>
-					<?php //} ?>
+		<div class="input_group">
+			<label for="meal[shopping_list]">Shopping list</label>
+			<textarea name="meal[shopping_list]" cols="75" rows="2" placeholder="Comma separated list. Eg: potatoes, olive oil, salt"></textarea>
+		</div>
+	</div>
+</form>
+<?php //} ?>
